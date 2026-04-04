366fcd118805df8c55b6e21fba70c021ffbb60a0
Arco993 <arne.coel1993@gmail.com>
2026-04-04 19:26:46 +0200

    Login verbeterd: Enter-toets werkt en foutmeldingen direct zichtbaar

--- app.py
+++ app.py
@@ -8,11 +8,15 @@ app.config['SECRET_KEY'] = 'jouw_geheime_sleutel_hier'
 
 db.init_app(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login' # Waar moet je heen als je niet bent ingelogd?
+login_manager.login_view = 'login'
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -21,27 +25,24 @@ def login():
         password = request.form.get('password')
         user = User.query.filter_by(username=username).first()
         
-        if user and user.password == password: # In een latere stap gaan we dit beveiligen (hashen)
+        if user and user.password == password:
             login_user(user)
             return redirect(url_for('dashboard'))
         else:
-            flash('Foutieve gebruikersnaam of wachtwoord')
+            flash('Oeps! Die combinatie van naam en wachtwoord klopt niet.')
             
     return render_template('login.html')
 
 @app.route('/dashboard')
 @login_required
 def dashboard():
-    return f"<h1>Welkom op het dashboard, {current_user.username}!</h1><p>Jouw rol is: {current_user.role}</p><a href='/logout'>Log uit</a>"
+    return render_template('dashboard.html')
 
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('login'))
-
-@app.route('/')
-def index():
+    flash('Je bent nu veilig uitgelogd.')
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
